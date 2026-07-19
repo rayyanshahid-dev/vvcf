c5355e59e58c15a3f82fe3e51876a88c365543fd
rayyanshahid-dev <rayyanshahid.dev@proton.me>
2026-07-19 19:35:45 +0800

    working on BAM parser in Julia

--- src/main.c
+++ src/main.c
@@ -2,5 +2,7 @@
 #include "../includes/header.h"
 
 int main(int argc, char* argv[]){
+
+	printf("Hello, world!\n");
     return 0;
 }
---
+++ src/parser.jl
@@ -0,0 +1,15 @@
+module BAMparser
+
+using XAM
+
+reader = open(BAM.Reader, "../data/C26B265CYP2C19_L00.bam")
+
+for record in reader
+	if BAM.ismapped(record)
+		println(BAM.refname(record), ':', BAM.position(record))
+	end 
+end
+
+close(reader)
+
+end
